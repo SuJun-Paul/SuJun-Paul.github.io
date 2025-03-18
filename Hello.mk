33e5f01e8d66d4332c1d1f5a13d4685066178aa8
채준수 <chaejunsu@chaejunsus-MacBook-Air.local>
2025-03-18 14:49:15 +0900

    DR_CHG

--- Hello.mk
+++ Hello.mk
@@ -1,3 +1,9 @@
-***
-* hi
-***
+git status : 확인
+
+git add main.py : main.py 파일추가
+git add . : 전체 추가
+
+git commit -m "수정내용 설명"
+
+git push origin main : main브랜치에 업로드. 다른 브랜치 작업이면 main대신 브랜치 명을 입력
+git pull origin main : github에서 최신 변경 사항을 로컬 	main 브랜치에 반영
